5a808c6d1bb81f7f4b1425cf65fbcf116f2f0c3b
hyeonmi411 <jewelkhm@naver.com>
2026-04-29 18:11:01 +0900

    day025-java-List(26.04.29)

--- track003_java/java2026/src/com/the703/days/Day023_.java
+++ track003_java/java2026/src/com/the703/days/Day023_.java
@@ -1,5 +1,37 @@
 package com.the703.days;
 
+//class A11 {
+//	int a; // (1)
+//	
+//	A11() { }
+//	
+//	A11(int a) { this.a = a; } // (2)
+//	
+//	//(3) void show()
+//	void show() { 
+//		this.a = 11; 
+//		System.out.println(this.a); 
+//	}
+//	
+//	//(4) static void classMethod()
+//	static void classMethod() { this.a = 12; }
+//	
+//	//(5) int showZ()
+//	int showZ() { 
+//		int a; 
+//		return a; 
+//	}
+//}
+//
+//public class Day023_ {
+//	public static void main(String[] args) {
+//
+//		int a;
+//		System.out.println(a);
+//		
+//	}
+//}
+
 class Parent7  extends Object{
 	int x = 100;
 	public Parent7() { super(); }
@@ -18,7 +50,7 @@ class Child7 extends Parent7 {
 		Child7  {x=200 , method()}  Child
  */
 
-public class 복습문제_2day023 {
+public class Day023_ {
 	public static void main(String[] args) {
 
 		Parent7 p = new Child7();     //부모  = 자식 (업캐스팅)
@@ -34,3 +66,4 @@ public class 복습문제_2day023 {
 				
 	}
 }
+
